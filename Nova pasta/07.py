b03a5568cd3c0dffc49041d38e2c126f9a35ec45
vloliveira <vitorloliveira96@gmail.com>
2025-04-22 19:46:47 -0300

    exercícios 7 e 8

--- Nova pasta/07.py
+++ Nova pasta/07.py
@@ -2,7 +2,6 @@
 Peça uma letra ao usuário e diga se é uma vogal (a, e, i, o, u) ou consoante."""
 
 letra = input("Digite uma letra: ")
-
 if letra == "a" or  letra == "e" or  letra == "i" or letra == "o" or letra == "u":
     print("É uma volgal")
 else:
---
+++ Nova pasta/08.py
@@ -0,0 +1,6 @@
+num = 16
+i = 1
+while i <= 5:
+    num *= 2
+    print(num)
+    i += 1
