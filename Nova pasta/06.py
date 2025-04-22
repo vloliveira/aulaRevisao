aa86ac0a3db01c56381a1d2b5dec78f9a5a1e8e4
vloliveira <vitorloliveira96@gmail.com>
2025-04-21 23:29:04 -0300

    Exercícios extras

---
+++ Nova pasta/06.py
@@ -0,0 +1,11 @@
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = 0
+if A == B:
+    soma = A + B
+    C = soma
+else:
+    multi = A * B
+    C = multi
+print(C)
+
