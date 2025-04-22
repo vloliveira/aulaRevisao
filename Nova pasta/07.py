299720f886658bcb8a2a78041ec24053bc4ee7d5
vloliveira <vitorloliveira96@gmail.com>
2025-04-22 15:30:03 -0300

    arquivo 07, atividade da volgal

---
+++ Nova pasta/07.py
@@ -0,0 +1,9 @@
+"""Verificador de Vogal
+Peça uma letra ao usuário e diga se é uma vogal (a, e, i, o, u) ou consoante."""
+
+letra = input("Digite uma letra: ")
+
+if letra == "a" or  letra == "e" or  letra == "i" or letra == "o" or letra == "u":
+    print("É uma volgal")
+else:
+    print("É consoante")
